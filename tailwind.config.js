bbfb26923500cc73eb8dcdff7f833bed5924cd5f
Dylan Hurtado López <dylahurtado43@gmail.com>
2022-06-19 04:37:14 +0200

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ["./public/src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'dbd-main-art':'url("../../img/dbd_keyart.jpg")',
+        'killer-ghostface':'url("../../img/killer-Ghostface.png")',
+        'killer-pig':'url("../../img/killer-pig.png")',
+        'survivor-leon':'url("../../img/survivor-leon.png")',
+        'survivor-cheryl':'url("../../img/survivor-cheryl.png")',
+        
+      }
+    },
+  },
+  plugins: [],
+}
